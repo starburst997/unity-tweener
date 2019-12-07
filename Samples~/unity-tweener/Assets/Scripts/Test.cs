473db43444daa48ddd0a2e37dda10f6646ddb210
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2019-12-07 15:53:44 -0500

    Test

---
+++ Samples~/unity-tweener/Assets/Scripts/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private bool _mouseClicked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
